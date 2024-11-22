698f83f679445e5c28a098314e84edf6bb78ce9f
Tezz Strömberg <kzefyowh@students.codeinstitute.net>
2024-11-22 16:36:10 +0000

    Moved if else statements out of the first function and created new functions for each for a more clean code.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,44 +6,69 @@ document.addEventListener("DOMContentLoaded", function () {
   let matchedPairs = [];
 
   // Player's turn
-  function flipCard(e) {
-    // Select a card
+  function clickedCard(e) {
     const card = e.currentTarget;
     console.log("Clicked card", card);
-    card.dataset.flipped = true;
 
+    const identifier = card.dataset.identifier;
+    matchedCard(identifier);
+
+    flipCard(card);
+    logCards(card, identifier);
+  }
+
+  // Select a card
+  function flipCard(card) {
+    card.dataset.flipped = true;
+  }
+
+  //Exit function if clicked card is part of an already matching pair
+  function matchedCard(identifier) {
+    return matchedPairs.includes(identifier);
+  }
+
+  function logCards(card, identifier) {
     // Is this first or second card?
     if (!cardOne) {
       // Store as first card
       cardOne = card;
       console.log("cardOne", cardOne);
     } else {
+      if (cardOne === card) {
+        return;
+      }
       // Store as second card
       cardTwo = card;
       console.log("cardTwo", cardTwo);
 
-      // Cards match?
-      if (cardOne.dataset.identifier === cardTwo.dataset.identifier) {
-        // Yes, keep cards face up
-        const identifier = card.dataset.identifier;
-        matchedPairs.push(identifier);
-        console.log("match", identifier);
-      } else {
-        // No, flip cards back down
-        cardOne.dataset.flipped = false;
-        cardTwo.dataset.flipped = false;
-        console.log("Flipping back");
-      }
-
-      // Reset card selection
-      cardOne = null;
-      cardTwo = null;
-      console.log("Reset turn");
+      matchCards(identifier);
     }
+  }
+
+  function matchCards(identifier) {
+    // Cards match?
+    if (cardOne.dataset.identifier === cardTwo.dataset.identifier) {
+      // Yes, keep cards face up
+      matchedPairs.push(identifier);
+      console.log("match", identifier);
+    } else {
+      // No, flip cards back down
+      cardOne.dataset.flipped = false;
+      cardTwo.dataset.flipped = false;
+      console.log("Flipping back");
+    }
+    resetCards();
+  }
+
+  function resetCards() {
+    // Reset card selection
+    cardOne = null;
+    cardTwo = null;
+    console.log("Reset turn");
   }
 
   // Walks through cards for eventlistener
   for (let index = 0; index < cards.length; index++) {
-    cards[index].addEventListener("click", flipCard);
+    cards[index].addEventListener("click", clickedCard);
   }
 });
